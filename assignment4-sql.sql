f994a9009bc9c692543d5c293ec684e7615337e8
ni-lie <cjay.coronel25@gmail.com>
2024-02-14 16:30:30 +0800

    upload: sql file for assignment 4

---
+++ assignment4-sql.sql
@@ -0,0 +1,6 @@
+SELECT TOP 10 ID, Team_Name, COUNT(table2.Winner_Team_ID) as NumberOfWins 
+FROM table1
+LEFT JOIN table2 ON table1.ID = table2.Winner_Team_ID
+GROUP BY table1.ID, table1.Team_Name
+ORDER BY NumberOfWins DESC
+
